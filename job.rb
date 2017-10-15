cdf3615849a4e917ebaf27df2379a2ca54c56593
Eugenia Grieff <megrieff@gmail.com>
2017-10-15 23:24:48 +0100

    Add methods to format input.

--- job.rb
+++ job.rb
@@ -2,7 +2,16 @@ class Job
 
   attr_accessor :name, :dependency
 
+  # Takes a line like "a => b" and returns @job = "a" and @dependency = "b"
+  # converting it first to and array and then removing white spaces.
   def initialize(input)
+    @name, @dependency = input.split(/ => ?/).map { |line| line.strip }
+  end
+
+  # If the is no dependency in the input string, the previous method will assign
+  # the value nil to @dependency so we need to check if before using it.
+  def has_dependency?
+    return true unless @dependency.nil?
   end
 
 end
--- sequence.rb
+++ sequence.rb
@@ -1,9 +1,19 @@
 class Sequence
 
   def initialize
+    @jobs = []
   end
 
+  def add(input)
+    input.split("\n").each do |line| # return each line as a string "b => c"
+      job = Job.new(line) # takes a line and convert it to an array and then assign it to @job and @dependecy as strings
+      @jobs.push(job) unless @jobs.include?(job) # push job instances to an array @jobs
+    end
+  end
+
+  # Will take array containing all job bjects and return array with new order.
   def ordered
+    # to do
   end
 
 end
